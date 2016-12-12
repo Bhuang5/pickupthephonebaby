c7be96eabcf35bcac171fcb54c59b6ec5dfbab67
Bhuang5 <Bhuang5@stuy.edu>
2016-12-12 01:20:28 -0500

    Add files via upload

--- control.c
+++ control.c
@@ -9,56 +9,6 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 
-/*
-int main(int argc, char *argv[]){
-  if (argc == 1) return 0;
-  if(!strncmp(argv[1], "-c", strlen(argv[1]))){
-    union semun uni;
-    int sem = semget(ftok("makefile", 122016), 1, IPC_CREAT | 0644 );
-    int mem = shmget( ftok("makefile", 2016), 4, IPC_CREAT | 0644);
-    uni.val = 1;
-    semctl(sem, 0, SETVAL, uni);
-    int file = open("read.txt", O_CREAT | O_TRUNC, 0644);
-    close(file);
-  } 
-
-  else if (!strncmp(argv[1], "-r", strlen(argv[1]))){
-    int sem = semget(ftok("makefile", 122016), 1, IPC_CREAT | 0644 );
-    int mem = shmget( ftok("makefile", 2016), 4, IPC_CREAT | 0644);
-    int file = open("read.txt", O_RDONLY, 0664);
-    struct stat *words = (struct stat*)malloc(sizeof(struct stat));
-    stat("read.txt", words);
-    if(words->st_size){
-      char call[words->st_size];
-      read(file, call, words->st_size);
-      printf("call transcript:\n%s\n", call);
-    }
-    close(file);
-    semctl(sem, 0, IPC_RMID);
-    shmctl(mem, IPC_RMID, 0);
-    free(words);
-  } 
-
-  else if (!strncmp(argv[1], "-v", strlen(argv[1]))){
-    int sem = semget(ftok("makefile", 122016), 1, IPC_CREAT | 0644 );
-    int val = semctl(sem, 0, GETVAL);
-    int file = open("read.txt", O_RDONLY, 0664);
-    struct stat *info = (struct stat*)malloc(sizeof(struct stat));
-    stat("read.txt", info);
-    if(info->st_size){
-      char message[info->st_size];
-      read(file, message, info->st_size);
-      printf("message: %s\n", message);
-    }
-    close(file);
-    free(info);
-  }
-  return 0;
-}
-*/
-
-
-
 int main(int argc, char *argv[]){
   int semid;
   int m;
--- telephone.c
+++ telephone.c
@@ -17,40 +17,32 @@ int main(int argc, char *argv[]){
   int sem = semget(ftok("makefile", 122016), 1, IPC_CREAT | 0644 );
   int mem = shmget( ftok("makefile", 2016), 4, IPC_CREAT | 0644);
 
-  char inputMes[2016];
+  char input[2016];
 
-  printf("Wait please, and let the other person talk!\n");
 
   struct sembuf buf = {0, -1, SEM_UNDO};
   //printf("%d\n",sem );
-  //semop(sem, &buf, 1);
-  //printf("%d\n",sem );
   int* memInfo = shmat(mem, 0, 0);
-  //printf("%d\n",sem );
   int readFile = open("read.txt", O_RDONLY);
   int writeFile = open("read.txt", O_CREAT | O_APPEND | O_WRONLY , 0644);
-  //printf("%d\n",sem );
   
   int mesLen = *(memInfo);
   char mes[mesLen + 1];
   lseek(readFile, -1 * mesLen, SEEK_END);
   read(readFile, mes, mesLen);
   mes[mesLen] = 0;
-  //printf("%d\n", mesLen);
+  
   close(readFile);
-  //printf("%d\n", mesLen);
+  
+  printf("Your message!\n");
+  printf("Message: %s\n",mes);
+  
 
-  printf("Message: ");
-  //if (mesLen < 2) printf("Your friend has nothing to say :P. Do you want to say anything?\n");
-  printf("%s \n", mes);
-
-
-  printf("Well, you heard what your friend said.\n");
-  printf("So, any response?: \n");
-  fgets(inputMes, 2016, stdin);
-  write(writeFile, inputMes, strlen(inputMes));
+  printf("What's your response? \n");
+  fgets(input, 2016, stdin);
+  write(writeFile, input, strlen(input));
   close(writeFile);
-  *memInfo = strlen(inputMes);
+  *memInfo = strlen(input);
 
   buf.sem_num = 1;
   semop(sem, &buf, 1);
