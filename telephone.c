eafd15e10a299caac2b81a8393b748b3697931f4
Bhuang5 <Bhuang5@stuy.edu>
2016-12-12 01:32:39 -0500

    Add files via upload

--- telephone.c
+++ telephone.c
@@ -12,19 +12,19 @@
 
 
 int main(int argc, char *argv[]){
-  int sem = semget(ftok("makefile", 122016), 1, IPC_CREAT | 0644 );
-  int mem = shmget( ftok("makefile", 2016), 4, IPC_CREAT | 0644);
+  int sem = semget(ftok("makefile", 1024), 1, IPC_CREAT | 0644 );
+  int mem = shmget( ftok("makefile", 1024), 4, IPC_CREAT | 0644);
 
-  char input[2016];
+  char input[1024];
 
 
   struct sembuf buf = {0, -1, SEM_UNDO};
   //printf("%d\n",sem );
-  int* memInfo = shmat(mem, 0, 0);
+  int* mem1 = shmat(mem, 0, 0);
   int readFile = open("read.txt", O_RDONLY);
   int writeFile = open("read.txt", O_CREAT | O_APPEND | O_WRONLY , 0644);
   
-  int mesLen = *(memInfo);
+  int mesLen = *(mem1);
   char mes[mesLen + 1];
   lseek(readFile, -1 * mesLen, SEEK_END);
   read(readFile, mes, mesLen);
@@ -37,10 +37,10 @@ int main(int argc, char *argv[]){
   
 
   printf("What's your response? \n");
-  fgets(input, 2016, stdin);
+  fgets(input, 1024, stdin);
   write(writeFile, input, strlen(input));
   close(writeFile);
-  *memInfo = strlen(input);
+  *mem1 = strlen(input);
 
   buf.sem_num = 1;
   semop(sem, &buf, 1);
